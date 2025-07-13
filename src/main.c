8356a614bf8b3d02966526fa0a277ba161b9ad36
hippocampa <tsdhrm@outlook.com>
2025-07-13 17:54:30 +0800

    refining init
    adding clang-format

--- src/main.c
+++ src/main.c
@@ -1,23 +1,41 @@
-#include "stdio.h"
-#include "string.h"
+#include <stdio.h>
+#include <string.h>
+#define HELP_CMD "help"
 
-void show_help(){
-    puts("Slugify");
-    puts("turning-string-to-slug\n");
-    puts("Usage:");
-    puts("slugify <string>\tTurn string to slug");
-    puts("slugify help\t\tShow help\n\n");
-    puts("Source: hippocampa.github.io");
+void
+process_name (char *arg)
+{
+  printf ("%s\n", arg);
 }
 
-int main(int argc, char *argv[]) {
-    if (argc == 1 || argc > 2){
-        show_help();
-    } else {
-        if (strcmp("help", argv[1]) == 0){
-            show_help();
+void
+show_help ()
+{
+  puts ("Slugify");
+  puts ("turning-string-to-slug\n");
+  puts ("Usage:");
+  puts ("slugify <string>\tTurn string to slug");
+  puts ("slugify help\t\tShow help\n\n");
+  puts ("Source: hippocampa.github.io");
+}
+
+int
+main (int argc, char *argv[])
+{
+  if (argc == 1 || argc > 2)
+    {
+      show_help ();
+    }
+  else
+    {
+      if (strcmp (HELP_CMD, argv[1]) == 0)
+        {
+          show_help ();
+        }
+      else
+        {
+          process_name (argv[1]);
         }
     }
-    return 0;
-
+  return 0;
 }
